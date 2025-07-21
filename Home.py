4ce639b25e87c6f9e37f7f3f7315abbaa5154bb8
Haider Ali <haiderkhalil@gmail.com>
2025-07-21 10:05:52 +0500

    2025-07-20-b

--- Home.py
+++ Home.py
@@ -35,7 +35,7 @@ if 'channels' not in st.session_state:
                 {"name": "DW News", "url": "https://www.youtube.com/watch?v=LuKwFajn37U"},
                 {"name": "GEO News", "url": "https://www.youtube.com/watch?v=O3DPVlynUM0"},
                 {"name": "ARY TV", "url": "https://www.youtube.com/watch?v=RqUZ2Fv9l8w"},
-                {"name": "AAJ News", "url": "https://www.youtube.com/watch?v=ZTA8eALsu4U"},
+                {"name": "AAJ News", "url": "https://www.youtube.com/watch?v=2Gub8-cSH9c"},
                 {"name": "SAMAA", "url": "https://www.youtube.com/watch?v=fB6Z4q01uFU"},
                 {"name": "Aljazeera", "url": "https://www.youtube.com/watch?v=gCNeDWCI0vo"},
             ]
